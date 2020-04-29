afba300e00253e7e7ddc558e6edd981d25cf49dc
Joel Spencer <joel@Joels-MacBook-Pro-3.local>
2020-04-29 01:39:58 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome("racecar"));
+            Console.WriteLine(IsPalindrome("racecars"));             
+        }
+
+        static string Reverse(string reverseString)
+        {
+            string Reversed = "";
+
+            for (int i = 0; i < reverseString.Length; i++)
+            {
+                Reversed = String.Concat(reverseString[i], Reversed);
+            }
+
+            return Reversed;
+        }
+
+        static bool IsPalindrome(string input)
+        {
+            string Reversed = Reverse(input);
+
+            bool isPalindrome = false;
+
+            if(Reversed == input)
+            {
+                isPalindrome = true;
+            }
+
+            return isPalindrome;            
+        }
+    }
+}
